e4fafbb1f251088000b8360ea22bca9325fa8dd4
liuqiming <john.liuqiming@gmail.com>
2022-11-11 17:48:16 +0800

    update wasm module

--- src/wasm/build.sh
+++ src/wasm/build.sh
@@ -1,5 +1,9 @@
-clang --target=wasm32-wasi \
+clang --target=wasm32-wasi -fPIC \
       --sysroot=${WASI_SDK_HOME}/share/wasi-sysroot \
-      -nostdlib -z stack-size=8192 -Wl,--initial-memory=65536 -Wl,--no-entry -Wl,--allow-undefined \
+      -nostdlib -z stack-size=8192 \
+      -Wl,--initial-memory=65536 \
+      -Wl,--no-entry \
+      -Wl,--allow-undefined \
       -Wl,--export=event \
+      -Wl,--export=buffer \
       -o sb_wasm_test.wasm sb_wasm_test.c
--- src/wasm/sb_wasm_test.c
+++ src/wasm/sb_wasm_test.c
@@ -1,3 +1,6 @@
+int buffer[1024];
+
 int event(int thread_id){
+    buffer[0] =1;
     return 0;
 }
