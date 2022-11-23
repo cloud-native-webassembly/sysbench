427fc2bc94ef61ed1cd4ed8a923bdfa0bc49673c
liuqiming <john.liuqiming@gmail.com>
2022-11-23 14:54:17 +0800

    refactor scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,3 @@
-
-export LD_LIBRARY_PATH=${LLVM_HOME}/lib64:${GCC_HOME}/lib64
-export PATH=${GCC_HOME}/bin:${PATH}
-export LANG=C 
 sh autogen.sh 
 ./configure --without-mysql --with-wamr --with-debug
 make clean 
--- scripts/debug.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=${LLVM_HOME}/lib64:${GCC_HOME}/lib64
-gdb -args ./src/sysbench --type=wasm --wasm-runtime=wamr --verbosity=5 --thread-init-timeout=0 src/wasm/sb_wasm_test.wasm run
